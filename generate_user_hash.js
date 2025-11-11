0c8c4a7ee0e1a0fe871ec46f12f055024845e8c2
estebanvx068-debug <estebanvx068@gmail.com>
2025-11-11 15:28:26 +0100

    Delete generate_user_hash.js

--- generate_user_hash.js
+++
@@ -1,9 +0,0 @@
-const crypto = require("crypto");
-if (process.argv.length < 4) {
-  console.log("Usage: node generate_user_hash.js <Pseudo> <Secret>");
-  process.exit(1);
-}
-const pseudo = process.argv[2];
-const secret = process.argv[3];
-const hash = crypto.createHash("sha256").update(secret).digest("hex");
-console.log(JSON.stringify({ pseudo, hash }, null, 2));
